3beea9a244bf2b6827f392b8c052b2d0dd874a00
Drew Fyock <fyockm@gmail.com>
2013-11-25 19:11:23 -0500

    hw 5-3 correct

--- hw5/hw5-3.js
+++ hw5/hw5-3.js
@@ -2,14 +2,14 @@ use test;
 db.grades.aggregate(
   { $unwind: "$scores" },
   { $match:
-    { "$scores.type": { $ne: "quiz" }}},
+    { "scores.type": { $ne: "quiz" }}},
   { $group:
     { _id: { student_id: "$student_id", class_id: "$class_id" },
-      avgStudentScore: { $avg: "$scores.score" }}},
+      avgScore: { $avg: "$scores.score" }}},
   { $group:
     { _id: "$_id.class_id",
-      avgClassScore: { $avg: "$avgStudentScore" }}},
-//  { $sort: { avgClassScore: -1 }},
-  { $sort: { avgClassScore: 1 }},
+      avgClassScore: { $avg: "$avgScore" }}},
+  { $sort: { avgClassScore: -1 }},
+//  { $sort: { avgClassScore: 1 }},
   { $limit: 3 }
 );
